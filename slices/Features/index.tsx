c1e12bc0f6b6dc6c08c60e7cc07ea11472126708
rbpolim <rbpolim@gmail.com>
2023-11-02 16:53:10 -0300

    feat: features slice

---
+++ slices/Features/index.tsx
@@ -0,0 +1,29 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+import { Bounded } from "@/components/bounded";
+import { FeaturesList } from "@/components/features-list";
+
+/**
+ * Props for `Features`.
+ */
+export type FeaturesProps = SliceComponentProps<Content.FeaturesSlice>;
+
+/**
+ * Component for "Features" Slices.
+ */
+const Features = ({ slice }: FeaturesProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <FeaturesList
+        title={slice.primary.title}
+        features={slice.items}
+      />
+    </Bounded>
+  );
+};
+
+export default Features;
