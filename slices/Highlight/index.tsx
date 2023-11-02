693b22ca5d17d5c3f6d084c5d02c04bf1739130a
rbpolim <rbpolim@gmail.com>
2023-11-02 16:53:45 -0300

    feat: highlight slice

---
+++ slices/Highlight/index.tsx
@@ -0,0 +1,79 @@
+import { Content } from "@prismicio/client";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Bounded } from "@/components/bounded";
+import { Heading } from "@/components/heading";
+import { CategoriesList } from "@/components/categories-list";
+
+/**
+ * Props for `Highlight`.
+ */
+export type HighlightProps = SliceComponentProps<Content.HighlightSlice>;
+
+/**
+ * Component for "Highlight" Slices.
+ */
+const Highlight = ({ slice }: HighlightProps): JSX.Element => {
+  const components: JSXMapSerializer = {
+    heading2: ({ children }) => <Heading>{children}</Heading>,
+    heading4: ({ children }) => (
+      <CardTitle className="text-lg md:text-xl">
+        {children}
+      </CardTitle>
+    ),
+  }
+
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.title} components={components} />
+
+      <div className="grid grid-cols-1 gap-4 mt-8 md:gap-6 md:grid-cols-3">
+        {slice.items.map((item, index) => {
+          const formatted = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(item.price!)
+
+          return (
+            <Card key={index}>
+              <CardHeader>
+                <div className="relative w-full h-40 overflow-hidden">
+                  <PrismicNextImage
+                    field={item.image}
+                    className="object-cover rounded-lg"
+                    fill
+                  />
+                </div>
+              </CardHeader>
+              <CardContent>
+                <PrismicRichText field={item.name} components={components} />
+                <CardDescription className="line-clamp-2">
+                  {item.description}
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <p>{formatted}</p>
+              </CardFooter>
+            </Card>
+          )
+        })}
+      </div>
+
+      <CategoriesList />
+    </Bounded>
+  );
+};
+
+export default Highlight;
